099adb8af3372a5e307995919a7aa2ef44cc8343
Travis Viet <idbaotoan@gmail.com>
2025-07-24 08:58:13 +0700

    Upload subpages
    Upload subpages:
    RFID Solutions
    Remote Crane Monitoring Solution
    Pump Monitoring System
    Fertilizer Production Process Monitoring
    Compressor Monitoring & Control Solution

--- Hitachi ICE_HTML original files/mpd/js/en/r1/script.js
+++ Hitachi ICE_HTML original files/mpd/js/en/r1/script.js
@@ -31,53 +31,64 @@ Copyright 2025 Hitachi, Ltd.
             var tabId = $(this).attr('data').replace('tab-', 'item-');
             $(this).parents('.tabs').find('.tab-item[data="' + tabId + '"]').addClass('active');
         });
-        //Slider
-        var $slider = $('.slider');
-        var $counter = $('.custom-counter');
-        $slider.on('init reInit afterChange', updateCounter);
-        $slider.slick({
-            slidesToShow: 4,
-            slidesToScroll: 4,
-            arrows: true,
-            dots: false,
-            adaptiveHeight: false,
-            prevArrow: $('.custom-prev'),
-            nextArrow: $('.custom-next'),
-            responsive: [
-                {
-                    breakpoint: 1024,
-                    settings: {
-                        slidesToShow: 2,
-                        slidesToScroll: 2,
-                    }
-                },
-                {
-                    breakpoint: 768.98,
-                    settings: {
-                        slidesToShow: 1,
-                        slidesToScroll: 1,
-                    }
-                }
-            ]
-        });
-        function updateCounter(event, slick, currentSlide) {
-            var currentIndex = Math.floor((currentSlide ? currentSlide : 0) / slick.options.slidesToScroll) + 1;
-            var totalPages = Math.ceil(slick.slideCount / slick.options.slidesToScroll);
-            $counter.text(currentIndex + '/' + totalPages);
-        }
-        $slider.trigger('init', [$slider.slick('getSlick')]);
-        //Fix equal height
-        function equalizeSlideHeights() {
-            let maxHeight = 0;
-            $('.articles.slider .slick-slide .article-inner').css('height', 'auto').each(function () {
-                let h = $(this).outerHeight();
-                if (h > maxHeight) maxHeight = h;
+        // Initialize all sliders
+        $('.slider').each(function (index, element) {
+            var $slider = $(element);
+            var $container = $(this); // assuming .articles is the container for each slider
+            var $counter = $container.parent().find('.custom-counter');
+            var $prev = $container.parent().find('.custom-prev');
+            var $next = $container.parent().find('.custom-next');
+
+            // Update counter
+            function updateCounter(event, slick, currentSlide) {
+                var currentIndex = Math.floor((currentSlide ? currentSlide : 0) / slick.options.slidesToScroll) + 1;
+                var totalPages = Math.ceil(slick.slideCount / slick.options.slidesToScroll);
+                $counter.text(currentIndex + '/' + totalPages);
+            }
+
+            // Equalize heights of slides in this slider
+            function equalizeSlideHeights() {
+                let maxHeight = 0;
+                $slider.find('.slick-slide .article-inner').css('height', 'auto').each(function () {
+                    let h = $(this).outerHeight();
+                    if (h > maxHeight) maxHeight = h;
+                });
+                $slider.find('.slick-slide .article-inner').height(maxHeight);
+            }
+
+            // Initialize Slick slider
+            $slider.on('init reInit afterChange', updateCounter);
+            $slider.on('setPosition', function () {
+                equalizeSlideHeights();
             });
-            $('.articles.slider .slick-slide .article-inner').height(maxHeight);
-        }
-        $('.articles.slider').on('setPosition', function () {
-            equalizeSlideHeights();
+
+            $slider.slick({
+                slidesToShow: 4,
+                slidesToScroll: 4,
+                arrows: true,
+                dots: false,
+                adaptiveHeight: false,
+                prevArrow: $prev,
+                nextArrow: $next,
+                responsive: [
+                    {
+                        breakpoint: 1024,
+                        settings: {
+                            slidesToShow: 2,
+                            slidesToScroll: 2,
+                        }
+                    },
+                    {
+                        breakpoint: 768.98,
+                        settings: {
+                            slidesToShow: 1,
+                            slidesToScroll: 1,
+                        }
+                    }
+                ]
+            });
         });
+
         //Back to top
         $(document).on('click', '#back-to-top', function (e) {
             e.preventDefault();
